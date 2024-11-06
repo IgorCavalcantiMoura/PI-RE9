74dbb351b2a1d8eafb2c3ed4ceeefe0583a5aeb2
IgorCavalcantiMoura <icavalcantimoura@gmail.com>
2024-11-06 17:21:08 -0300

    :bug: fix: Corrigindo erros no deploy

--- src/aplicacoes/aplicacoes.module.ts
+++ src/aplicacoes/aplicacoes.module.ts
@@ -7,9 +7,10 @@ import { CandidatoModule } from '../candidatos/candidatos.module';
 import { VagasModule } from '../vagas/vagas.module';
 import { CandidatoService } from '../candidatos/services/candidato.service';
 import { VagaService } from '../vagas/services/vagas.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Aplicacao]), CandidatoModule, VagasModule],
+    imports: [TypeOrmModule.forFeature([Aplicacao]), CandidatoModule, VagasModule, HttpModule],
     providers: [AplicacaoService, CandidatoService, VagaService],
     controllers: [AplicacaoController],
     exports: [TypeOrmModule]
