f0271aac0fafe242d5b27027d123354d170651e0
IgorCavalcantiMoura <icavalcantimoura@gmail.com>
2024-11-06 17:07:27 -0300

    :rocket: deploy

--- src/aplicacoes/aplicacoes.module.ts
+++ src/aplicacoes/aplicacoes.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Aplicacao } from './entities/aplicacao.entity';
+import { AplicacaoService } from './services/aplicacao.service';
+import { AplicacaoController } from './controllers/aplicacao.controller';
+import { CandidatoModule } from '../candidatos/candidatos.module';
+import { VagasModule } from '../vagas/vagas.module';
+import { CandidatoService } from '../candidatos/services/candidato.service';
+import { VagaService } from '../vagas/services/vagas.service';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([Aplicacao]), CandidatoModule, VagasModule],
+    providers: [AplicacaoService, CandidatoService, VagaService],
+    controllers: [AplicacaoController]
+})
 export class AplicacoesModule {}
