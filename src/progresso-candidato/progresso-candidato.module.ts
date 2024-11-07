21ed0e7fc4873581d1dce6f8acf4eae4d8033288
IgorCavalcantiMoura <icavalcantimoura@gmail.com>
2024-11-06 23:59:35 -0300

    :sparkles: feat: Modulos Fase e Progresso do Candidato

--- src/progresso-candidato/progresso-candidato.module.ts
+++ src/progresso-candidato/progresso-candidato.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProgressoCandidato } from './entities/progressoCandidato.entity';
 import { ProgressoCandidatoService } from './services/progressoCandidato.service';
 import { ProgressoCandidatoController } from './controllers/progressoCandidato.controller';
+import { AplicacoesModule } from '../aplicacoes/aplicacoes.module';
+import { AplicacaoService } from '../aplicacoes/services/aplicacao.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([ProgressoCandidato])],
-    providers: [ProgressoCandidatoService],
+    imports: [TypeOrmModule.forFeature([ProgressoCandidato]), AplicacoesModule],
+    providers: [ProgressoCandidatoService, AplicacaoService],
     controllers: [ProgressoCandidatoController],
     exports: [TypeOrmModule]
 })
