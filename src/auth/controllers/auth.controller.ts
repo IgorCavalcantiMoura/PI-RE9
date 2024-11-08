4461fd234a4518dd4abff0fa488847317ee6b6e4
IgorCavalcantiMoura <icavalcantimoura@gmail.com>
2024-11-07 22:32:46 -0300

    :recycle: refactor: Modulo Auth

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Post, Body, Request, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from '../services/auth.service';
 import { EmpresaLocalAuthGuard } from '../guard/empresa-local-auth-guard';
 import { CandidatoLocalAuthGuard } from '../guard/candidato-local-auth-guard';
-
 
 @Controller('auth')
 export class AuthController {
@@ -10,12 +10,36 @@ export class AuthController {
 
   @UseGuards(EmpresaLocalAuthGuard)
   @Post('login/empresa')
+  @ApiOperation({ summary: 'Autenticar empresa' })
+  @ApiResponse({
+    status: 200,
+    description: 'Login bem-sucedido para a empresa',
+    schema: {
+      type: 'object',
+      properties: {
+        token: { type: 'string' },
+      },
+    },
+  })
+  @ApiResponse({ status: 401, description: 'Credenciais inválidas' })
   async loginEmpresa(@Request() req) {
     return this.authService.login(req.user, 'empresa');
   }
 
   @UseGuards(CandidatoLocalAuthGuard)
   @Post('login/candidato')
+  @ApiOperation({ summary: 'Autenticar candidato' })
+  @ApiResponse({
+    status: 200,
+    description: 'Login bem-sucedido para o candidato',
+    schema: {
+      type: 'object',
+      properties: {
+        token: { type: 'string' },
+      },
+    },
+  })
+  @ApiResponse({ status: 401, description: 'Credenciais inválidas' })
   async loginCandidato(@Request() req) {
     return this.authService.login(req.user, 'candidato');
   }
--- src/auth/controllers/empresa-auth.controller.ts
+++ src/auth/controllers/empresa-auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 import { EmpresaAuthService } from '../services/empresa-auth.service';
 
 @Controller('auth/empresa')
@@ -6,6 +7,30 @@ export class EmpresaAuthController {
   constructor(private readonly empresaAuthService: EmpresaAuthService) {}
 
   @Post('login')
+  @ApiOperation({ summary: 'Autenticar empresa' })
+  @ApiBody({
+    description: 'Informações de login da empresa',
+    type: Object,
+    examples: {
+      loginExample: {
+        value: {
+          email: 'empresa@exemplo.com',
+          senha: 'senha123',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Login bem-sucedido',
+    schema: {
+      type: 'object',
+      properties: {
+        token: { type: 'string' },
+      },
+    },
+  })
+  @ApiResponse({ status: 400, description: 'Credenciais inválidas' })
   async login(@Body('email') email: string, @Body('senha') senha: string) {
     const empresa = await this.empresaAuthService.validarEmpresa(email, senha);
     if (!empresa) {
